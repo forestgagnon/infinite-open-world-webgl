238287ede0d28812249d36eb32579a248f41fe9e
Forest Gagnon <forestgagnon@gmail.com>
2016-11-26 13:36:06 -0500

    barebones setup

--- assignment7.js
+++ assignment7.js
@@ -24,22 +24,6 @@ void main(){
   gl_FragColor = v_Color;
 }`;
 
-/*
-  This creates the scenegraph. Calling this returns the root node.
-
-  Note that the scenegraph has a stack and a current transformation.
-
-  There are two kinds of nodes, shape and transformation.
-
-  The transformation nodes take in a transformation matrix as data. They also have two functions:
-    add(type data) - creates a new node of type "type", adds it to its child list and returns it.
-    apply() - applies its associated transformation by multiplying it with the current matrix. Calls apply on all children
-
-  The shape node takes in a function to be called to draw the associated shape. It has one function:
-    apply() - calls its associated drawing method to draw the shape with the current transformation.
-
-*/
-
 function rgbToFloats(r, g, b) {
   return { r: r/255, g: g/255, b: b/255 };
 }
@@ -79,11 +63,6 @@ var createScenegraph = function(gl, program){
         return node;
       },
       apply: () =>{
-        /* YOUR CODE HERE */
-        /* This needs to multiply in the node's matrix with the current transform and then iterate over all of the children, calling their apply() functions.
-
-        Make use of the stack to preserve the state of the current matrix.
-        */
 
         stack.push(mat4.clone(currentMatrix));
         let newMatrix = mat4.create();
@@ -201,70 +180,6 @@ const createCamera = function(gl, program) {
   }
 };
 
-//========== OBJECT PLACEMENT ==========\\
-function generateObjectLocations() {
-  let allLocations = {};
-  for(x = -HALF_GRID_SIZE; x < HALF_GRID_SIZE; x+=2) {
-    for(z = -HALF_GRID_SIZE; z < HALF_GRID_SIZE; z+=2) {
-      allLocations[x.toString() + '--' + z.toString()] = {x: x, z: z};
-    }
-  }
-  let cubeLocations = [];
-  let pyramidLocations = [];
-  let frustumLocations = [];
-  let composedStackLocations = [];
-  while(Object.keys(allLocations).length > 0) {
-    let randomLocation = allLocations[Object.keys(allLocations)[getRandomInt(0, Object.keys(allLocations).length - 1)]];
-    switch(getRandomInt(0, 4)) {
-      case 0:
-        cubeLocations.push({
-          position: vec3.fromValues(randomLocation.x, 0, randomLocation.z),
-          scale: vec3.fromValues(1, getRandom(0.1, 2), 1)
-        });
-        break;
-
-      case 1:
-        pyramidLocations.push({
-          position: vec3.fromValues(randomLocation.x, 0, randomLocation.z),
-          scale: vec3.fromValues(1, getRandom(0.1, 5), 1)
-        });
-        break;
-
-      case 2:
-        frustumLocations.push({
-          position: vec3.fromValues(randomLocation.x, 0, randomLocation.z),
-          scale: vec3.fromValues(1, getRandom(0.1, 1), 1)
-        });
-        break;
-
-      case 3:
-      let isAnimated = getRandomInt(0,1) === 1;
-      let numSteps = getRandomInt(20, 50);
-      let minStep = getRandomInt(10,  Math.floor(numSteps - numSteps/4));
-        composedStackLocations.push({
-          position: vec3.fromValues(randomLocation.x, 0, randomLocation.z),
-          scale: vec3.fromValues(1, getRandom(0.1, 1), 1),
-          animate: isAnimated,
-          a_speed: getRandom(0.01, 0.2),
-          a_currentStep: getRandomInt(minStep, numSteps),
-          a_currentDirection: ['forward', 'backward'][getRandomInt(0, 1)],
-          a_numSteps: numSteps,
-          a_minStep: minStep
-
-        });
-        break;
-    }
-
-    delete allLocations[randomLocation.x + '--' + randomLocation.z];
-  }
-
-  return {
-    cubeLocations: cubeLocations,
-    pyramidLocations: pyramidLocations,
-    frustumLocations: frustumLocations,
-    composedStackLocations: composedStackLocations
-  }
-}
 
 //========== DRAWING FUNCTION GENERATORS ==========\\
 function createGrid(gl, program) {
@@ -363,126 +278,6 @@ function createCube(gl, program) {
   };
 }
 
-function createPyramid(gl, program) {
-  let pyramid = {
-    vertices : new Float32Array([
-          0.0, 0.0,  0.0,
-          0.0,  0.0,  1.0,
-          1.0,  0.0,  0.0,
-          1.0,  0.0,  1.0,
-
-          0.5,  1.0,  0.5
-    ]),
-    colors: new Float32Array([
-          DARK_RED.r, DARK_RED.g, DARK_RED.b,
-          DARK_RED.r, DARK_RED.g, DARK_RED.b,
-          DARK_RED.r, DARK_RED.g, DARK_RED.b,
-          DARK_RED.r, DARK_RED.g, DARK_RED.b,
-
-          BURNT_ORANGE.r, BURNT_ORANGE.g, BURNT_ORANGE.b
-    ]),
-
-    indices: new Uint8Array([
-       0,1,2,  1,2,3, // bottom face
-       0,1,4, //
-       0,2,4, //
-       1,3,4, //
-       2,3,4, //
-
-    ]),
-    dimensions: 3,
-    numPoints: 5
-  };
-  pyramid.vertexBuffer = gl.createBuffer();
-  pyramid.colorBuffer = gl.createBuffer();
-  pyramid.indexBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, pyramid.vertexBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, pyramid.vertices, gl.STATIC_DRAW);
-
-  gl.bindBuffer(gl.ARRAY_BUFFER, pyramid.colorBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, pyramid.colors, gl.STATIC_DRAW);
-
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, pyramid.indexBuffer);
-  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, pyramid.indices, gl.STATIC_DRAW);
-
-  return () => {
-    gl.bindBuffer(gl.ARRAY_BUFFER, pyramid.vertexBuffer);
-    // associate it with our position attribute
-    gl.vertexAttribPointer(program.a_Position, pyramid.dimensions, gl.FLOAT, false, 0,0);
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, pyramid.colorBuffer);
-    // associate it with our position attribute
-    gl.vertexAttribPointer(program.a_Color, pyramid.dimensions, gl.FLOAT, false, 0,0);
-
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, pyramid.indexBuffer);
-    gl.drawElements(gl.TRIANGLES, pyramid.indices.length, gl.UNSIGNED_BYTE, 0);
-  };
-}
-
-function createFrustum(gl, program) {
-  let frustum = {
-    vertices : new Float32Array([
-          0.75,  1.0,  0.75,
-          0.25,  1.0,  0.75,
-          0.0,  0.0,  1.0,
-          1.0, 0.0,  1.0,
-
-          0.75,  1.0,  0.25,
-          0.25,  1.0,  0.25,
-          0.0,  0.0,  0.0,
-          1.0,  0.0,  0.0
-    ]),
-    colors: new Float32Array([
-      SKY_BLUE.r, SKY_BLUE.g, SKY_BLUE.b,
-      SKY_BLUE.r, SKY_BLUE.g, SKY_BLUE.b,
-      DARK_BLUE.r, DARK_BLUE.g, DARK_BLUE.b,
-      DARK_BLUE.r, DARK_BLUE.g, DARK_BLUE.b,
-
-      SKY_BLUE.r, SKY_BLUE.g, SKY_BLUE.b,
-      SKY_BLUE.r, SKY_BLUE.g, SKY_BLUE.b,
-      DARK_BLUE.r, DARK_BLUE.g, DARK_BLUE.b,
-      DARK_BLUE.r, DARK_BLUE.g, DARK_BLUE.b
-
-    ]),
-
-    indices: new Uint8Array([
-       0,1,2,  0,2,3, // front face
-       0,7,4,  0,3,7,   // right face
-       1,5,6,  1,6,2, // left face
-       0,4,5,  0,5,1, // top face
-       3,2,6,  3,6,7, // bottom face
-       4,7,6,  4,6,5 // back facew
-
-    ]),
-    dimensions: 3,
-    numPoints: 8
-  };
-  frustum.vertexBuffer = gl.createBuffer();
-  frustum.colorBuffer = gl.createBuffer();
-  frustum.indexBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, frustum.vertexBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, frustum.vertices, gl.STATIC_DRAW);
-
-  gl.bindBuffer(gl.ARRAY_BUFFER, frustum.colorBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, frustum.colors, gl.STATIC_DRAW);
-
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, frustum.indexBuffer);
-  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, frustum.indices, gl.STATIC_DRAW);
-
-  return () => {
-    gl.bindBuffer(gl.ARRAY_BUFFER, frustum.vertexBuffer);
-    // associate it with our position attribute
-    gl.vertexAttribPointer(program.a_Position, frustum.dimensions, gl.FLOAT, false, 0,0);
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, frustum.colorBuffer);
-    // associate it with our position attribute
-    gl.vertexAttribPointer(program.a_Color, frustum.dimensions, gl.FLOAT, false, 0,0);
-
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, frustum.indexBuffer);
-    gl.drawElements(gl.TRIANGLES, frustum.indices.length, gl.UNSIGNED_BYTE, 0);
-  };
-}
-
 //========== MAIN ONLOAD FUNCTION ==========\\
 window.onload = function(){
 
@@ -512,10 +307,7 @@ window.onload = function(){
   let camera = createCamera(gl, program);
 
 
-  /*
-  The conventional key handler detects when a key is held down for repeat actions, but it has a pause before it detects the repeat and it is flaky with two keys held down simultaneously. This avoids this by maintaining a mapping of the keys that are currently pressed.
-  */
-  var keyMap = {};
+  let keyMap = {};
 
   window.onkeydown = function(e){
       keyMap[e.which] = true;
@@ -525,12 +317,6 @@ window.onload = function(){
        keyMap[e.which] = false;
   }
 
-  objectLocations = generateObjectLocations();
-
-  let animationDirection = 'forward';
-  let currentStep = 0;
-
-  // the render function
   let render = function(){
 
     // check which keys that we care about are down
@@ -579,80 +365,9 @@ window.onload = function(){
     //Initialize scenegraph and drawing functions
     let rootNode = createScenegraph(gl, program);
     let grid = createGrid(gl, program);
-    let cube = createCube(gl, program);
-    let pyramid = createPyramid(gl, program);
-    let frustum = createFrustum(gl, program);
 
     //Place grid
     let gridNode = rootNode.add("shape", grid);
-
-    //Place Cubes
-    objectLocations.cubeLocations.forEach((location) => {
-      let transform = mat4.create();
-      mat4.translate(transform, mat4.create(), location.position);
-      mat4.scale(transform, transform, location.scale);
-      let transformed = rootNode.add("transformation", transform);
-      transformed.add("shape", cube);
-    });
-
-    //Place Pyramids
-    objectLocations.pyramidLocations.forEach((location) => {
-      let transform = mat4.create();
-      mat4.translate(transform, mat4.create(), location.position);
-      mat4.scale(transform, transform, location.scale);
-      let transformed = rootNode.add("transformation", transform);
-      transformed.add("shape", pyramid);
-    });
-
-    //Place Frustums
-    objectLocations.frustumLocations.forEach((location) => {
-      let transform = mat4.create();
-      mat4.translate(transform, mat4.create(), location.position);
-      mat4.scale(transform, transform, location.scale);
-      let transformed = rootNode.add("transformation", transform);
-      transformed.add("shape", frustum);
-    });
-
-    //place Composed Stacks
-    objectLocations.composedStackLocations.forEach((location) => {
-      let animationScaleVector;
-      if (location.animate) {
-        animationScaleVector = vec3.fromValues(1, easeInOutQuad(location.a_currentStep, 0, location.a_speed * location.a_numSteps, location.a_numSteps), 1);
-        if (location.a_currentDirection === 'forward' && location.a_currentStep === location.a_numSteps) {
-          location.a_currentDirection = 'backward';
-        }
-        else if (location.a_currentDirection === 'backward' && location.a_currentStep === location.a_minStep) {
-          location.a_currentDirection = 'forward';
-        }
-        location.a_currentDirection === 'forward' ? location.a_currentStep++ : location.a_currentStep--;
-      }
-
-      let translate = mat4.create();
-      mat4.translate(translate, mat4.create(), location.position);
-      let translated = rootNode.add("transformation", translate);
-
-      let scale = mat4.create();
-      mat4.scale(scale, mat4.create(), location.scale);
-      let scaled = translated.add("transformation", scale);
-
-      scaled.add('shape', frustum);
-
-      let upperScaleFactor = vec3.create();
-      vec3.mul(upperScaleFactor, vec3.fromValues(0.25, 0.5, 0.25), location.scale);
-      let upperTransform = mat4.create();
-      mat4.translate(upperTransform, mat4.create(), vec3.fromValues(upperScaleFactor[0] + upperScaleFactor[0]/2, location.scale[1], upperScaleFactor[2] + upperScaleFactor[2]/2));
-      mat4.scale(upperTransform, upperTransform, upperScaleFactor);
-      if (location.animate) {
-        mat4.scale(upperTransform, upperTransform, animationScaleVector);
-      }
-      let upperTransformed = translated.add("transformation", upperTransform);
-      upperTransformed.add('shape', cube);
-
-      let topTransform = mat4.create();
-      mat4.translate(topTransform, mat4.create(), vec3.fromValues(0, 1, 0));
-      let topTransformed = upperTransformed.add('transformation', topTransform);
-      topTransformed.add('shape', pyramid);
-    });
 
     rootNode.apply();
 
